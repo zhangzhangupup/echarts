ee44ecc4a357074454d337a0541232a9cd4d143d
zhangzhangupup <2083922912@qq.com>
2022-03-25 09:49:40 +0800

    新的分支添加一些东西

--- Echarts-provider/src/main/java/com/zhang/service/Impl/NumberOfCasesInStockServiceImpl.java
+++ Echarts-provider/src/main/java/com/zhang/service/Impl/NumberOfCasesInStockServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
 public class NumberOfCasesInStockServiceImpl implements NumberOfCasesInStockService {
+    //自动注入
     @Autowired
     private NumberOfCasesInStockDao numberOfCasesInStockDao;
     @Override
